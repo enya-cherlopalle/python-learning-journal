8cd09278397f3798f10ef286dd5d17e0b0cb69b1
enya-cherlopalle <111300635+enya-cherlopalle@users.noreply.github.com>
2024-09-08 14:24:42 -0700

    Add files via upload

---
+++ Lab2.py
@@ -0,0 +1,17 @@
+#Task 1: Kilometer to Mile Conversion
+
+kilometer = input("Enter a distance in kilometers: ")
+miles = float(kilometer)*0.621371
+print(miles)
+
+#Task 2: Celsius to Fahrenheit Conversion
+Celsius = input("Enter a temperature in Celsius: ")
+Fahrenheit = (float(Celsius)*9/5)+32
+print(Fahrenheit)
+
+#Task 3: Binary to Decimal Converter
+#Convert 101101000 into decimal
+
+decimal = (1 * 2**8) + (0 * 2**7) + (1 * 2**6) + (1 * 2**5) + (0 * 2**4) + (1 * 2**3) + (0 * 2**2) + (0 * 2**1) + (0 * 2**0)
+
+print("101101000 into decimal is: ", decimal)
